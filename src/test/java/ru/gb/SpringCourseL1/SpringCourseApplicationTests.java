57cb2035a9ee3da49f465e444ba22ae31f055405
SKoksharov <konsist-koksharovsa@yandex.ru>
2024-01-19 10:58:10 +0700

    First Commit

---
+++ src/test/java/ru/gb/SpringCourseL1/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.SpringCourseL1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
